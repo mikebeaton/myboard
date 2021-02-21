247ddab9193643dae005c3558dd38f3ee63f6aa8
MikeBeaton <mjsbeaton@gmail.com>
2021-02-21 20:33:29 +0000

    myboard.sh: no need to work from own directory

--- myboard.sh
+++ myboard.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# cd to own dir in case we are run from a symlink
-SELF_PATH=$(perl -MCwd -le 'print Cwd::abs_path shift' "$0")&& SELF_PATH=$(dirname $SELF_PATH)
-cd $SELF_PATH
+# change dir to OC model DB dir
+cd ~/OpenSource/OpenCorePkg/AppleModels/DataBase
 
-# get board-id into a shell var
+# get board-id into shell var
 eval "$(ioreg -l | grep board-id | awk -F '[><]' '{print "MyBoard=" $2}')"
 
-echo Board-ID: \"$MyBoard\"
+# show detected board id
+echo Board ID: \"$MyBoard\"
 
-# show the matching SystemProductName from each OC .yaml file file which contains board-id
-grep -rnl ../OpenCorePkg/AppleModels/DataBase -e $MyBoard | xargs grep -w -e SystemProductName
+# show the matching SystemProductName from each OC .yaml file file which contains board id
+grep -rnl . -e $MyBoard | xargs grep -w -e SystemProductName
