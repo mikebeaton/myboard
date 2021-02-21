1182cd4206ce1f3a549e80c81eaa01684e0ced14
MikeBeaton <mjsbeaton@gmail.com>
2021-02-21 09:32:11 +0000

    Make this work even if you run it via a symlink

--- myboard.sh
+++ myboard.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SELF_PATH=$(perl -MCwd -le 'print Cwd::abs_path shift' "$0")&& SELF_PATH=$(dirname $SELF_PATH)
+cd $SELF_PATH
+
 eval "$(ioreg -l | grep board-id | awk -F '[><]' '{print "MyBoard=" $2}')"
 echo $MyBoard
 cd ../OpenCorePkg/AppleModels/DataBase
